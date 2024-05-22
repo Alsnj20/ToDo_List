ec292e2c60765a975e2572f820f553a9c4a7fd13
Alsnj20 <marieljara656@gmail.com>
2024-05-22 00:16:33 -0500

    class Projects

---
+++ src/project.js
@@ -0,0 +1,15 @@
+class Project{
+  constructor(name){
+    this.name = name;
+    this.tasks = [];
+  }
+  addTask(task){
+    this.tasks.push(task);
+  }
+  removeTask(task){
+    return this.tasks.filter((t) => t !== task);
+  }
+  getTasks(){
+    return this.tasks;
+  }
+}
