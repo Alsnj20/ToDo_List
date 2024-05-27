530a927ae4260cdec426bab24055cbc569e76412
Alsnj20 <marieljara656@gmail.com>
2024-05-26 23:32:30 -0500

    Escructura del style

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,19 @@
 const img = document.querySelector('.toggle-button img');
-
+const navItem = document.querySelector('.content-nav');
+const body = document.querySelector('body');
 img.addEventListener('click', () => {
   console.log('clicked');
   if(img.classList.contains('dark')) {
     img.src = './img/sunny.png';
     img.classList.remove('dark');
     img.classList.add('active');
+    navItem.style.backgroundColor = '#DFBCC8';
+    navItem.style.color = '#151515';
   }else{
     img.classList.add('dark');
     img.classList.remove('active');
     img.src = './img/moon.png';
+    navItem.style.backgroundColor = '#151515';
+    navItem.style.color = '#ffffff';
   }
 })
