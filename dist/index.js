8c0b4a280d3f826c3b8ce4acfb7b1d5bafcfc1e1
Alsnj20 <marieljara656@gmail.com>
2024-06-06 08:31:36 -0500

    toggle funcionable

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,13 @@
 const img = document.querySelector('.toggle-button img');
-const navItem = document.querySelector('.content-nav');
 const body = document.querySelector('body');
 img.addEventListener('click', () => {
-  console.log('clicked');
+  img.classList.toggle("dark");
+  body.classList.toggle("dark");
+  
   if(img.classList.contains('dark')) {
-    img.src = './img/sunny.png';
-    img.classList.remove('dark');
-    img.classList.add('active');
-    navItem.style.backgroundColor = '#DFBCC8';
-    navItem.style.color = '#151515';
-  }else{
-    img.classList.add('dark');
-    img.classList.remove('active');
     img.src = './img/moon.png';
-    navItem.style.backgroundColor = '#151515';
-    navItem.style.color = '#ffffff';
+
+  }else{
+    img.src = './img/sunny.png';
   }
 })
