b443e344ca86b52c7559b165cf28db3d1abf70fc
Alsnj20 <marieljara656@gmail.com>
2024-06-06 09:42:55 -0500

    Menu en tres lineas

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 const img = document.querySelector('.toggle-button img');
 const body = document.querySelector('body');
+const menu = document.querySelector('.menu');
+const nav = document.querySelector('.content-nav div:first-child')
 img.addEventListener('click', () => {
   img.classList.toggle("dark");
   body.classList.toggle("dark");
@@ -9,5 +11,14 @@ img.addEventListener('click', () => {
 
   }else{
     img.src = './img/sunny.png';
+  }
+})
+
+menu.addEventListener('click', () => {
+  const isHidden = nav.classList.toggle('active');
+  if (isHidden) {
+    menu.classList.replace('bx-menu', 'bx-x');
+  } else {
+    menu.classList.replace('bx-x', 'bx-menu');
   }
 })
