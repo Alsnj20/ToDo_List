0f4277bf572bb360d5d93b626adbb22584d03e83
Alsnj20 <marieljara656@gmail.com>
2024-06-21 18:10:33 -0500

    list Projects

--- src/todoList.js
+++ src/todoList.js
@@ -1,21 +1,40 @@
+//Add Project
 const addProject = document.querySelector('#addProject');
-console.log(addProject);
 const formProject = document.querySelector('#formProject');
 const inProject = document.querySelector('#formProject input');
 const btnDoProject = document.querySelector('#doProject');
 const btnCancelProject = document.querySelector('#cancelProject');
-addProject.addEventListener('click', ()=> {
+addProject.addEventListener('click', () => {
   formProject.style.display = 'block';
 })
 
-btnCancelProject.addEventListener('click', ()=> {
+btnCancelProject.addEventListener('click', () => {
   formProject.style.display = 'none';
 })
 
-btnDoProject.addEventListener('click', ()=> {
-  console.log(inProject.value);
-  formProject.style.display = 'none';
-  inProject.value = '';
+btnDoProject.addEventListener('click', () => {
+  if (inProject.value !== '') {
+    createProject(inProject.value);
+    formProject.style.display = 'none';
+    inProject.value = '';
+  }
 });
+
+//Projects
+const titleProject = document.querySelector('#titleProject');
+const projects = document.querySelector('.sectionList ul');
+
+
+const createProject = (title) => {
+  const li = document.createElement('li');
+  li.innerHTML = `<a href="#"><i class='bx bx-list-plus'></i>${title}</a><i class='bx bx-dots-vertical-rounded'></i>`;
+  projects.appendChild(li);
+}
+
+
+//add Task
+
+
+//Task
 
 
